4bb9847779ebdcd9f1ccf512f6832c0aae367619
Leslie A. Hurtado <lesliehurtado1103@gmail.com>
2021-09-17 19:14:13 -0500

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-$( document ).ready(function() {
-    console.log( "ready!" );
-    $( "#thefool" ).click(function() {
-        $("#thefool").toggleClass("flipsidefool");
-        $("#authdesc").toggleClass("hidden");
-        $("#webdesc").toggleClass("hidden");
-});
-});
